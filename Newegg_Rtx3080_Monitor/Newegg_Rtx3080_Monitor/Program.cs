a0afd2cff71675b0729176ba2b4de6c30b555dff
Ryan Swaby <ryan.swaby@gmail.com>
2020-09-26 11:23:46 -0400

    initial project upload

---
+++ Newegg_Rtx3080_Monitor/Newegg_Rtx3080_Monitor/Program.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace Newegg_Rtx3080_Monitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            bool keepChecking = true;
+            while (keepChecking)
+            {
+                // This will open up the URL 
+                //driver.Url = "https://www.newegg.com/p/pl?d=rtx+3070+super";
+                driver.Url = "https://www.newegg.com/p/pl?d=rtx+3080";
+
+                try
+                {
+                    // Look for button with Add to cart text
+                    var addToCartButton = driver.FindElement(By.XPath("//button[.=\"Add to cart \"]"));
+
+                    // Alert beeps
+                    for (int i = 0; i < 30; i++)
+                    {
+                        Console.Beep();
+                        Console.Beep();
+                        Console.Beep();
+                        Thread.Sleep(2000);
+                    }
+                    // end loop
+                    keepChecking = false;
+                }
+                catch (Exception)
+                {
+                    Thread.Sleep(20000);
+                    driver.Navigate().Refresh();
+                }
+
+            }
+
+            driver.Close();
+            driver.Dispose();
+          
+        }
+    }
+}
